9e99b618ed3f3600bc7ec361b395711333a6e4e8
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-07-17 14:34:30 +0100

    Use kg to generate manifest

--- operator/tools.go
+++ operator/tools.go
@@ -4,5 +4,6 @@ package main
 
 // see https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 import (
+	_ "github.com/errordeveloper/kue/cmd/kg"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
