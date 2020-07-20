4b8158204edc1688525b498a9775b90c4281161a
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-07-20 16:03:45 +0100

    Use kg to generate real deployment as well as test job

---
+++ operator/config/operator/template.cue
@@ -0,0 +1,132 @@
+constants: {
+	name: "gke-test-cluster-operator"
+}
+
+_workload: {
+	metadata: {
+		name: "\(constants.name)"
+		labels: name: "\(constants.name)"
+		namespace: "\(parameters.namespace)"
+	}
+	spec: _workloadSpec
+}
+
+_workloadSpec: {
+	template: {
+		metadata: labels: name: "\(constants.name)"
+		spec: {
+			serviceAccount: "\(constants.name)"
+			volumes: [{
+				name: "tmp"
+				emptyDir: {}
+			}]
+			containers: [{
+				command: _command
+				image:   "\(parameters.image)"
+				name:    "operator"
+				volumeMounts: [{
+					name:      "tmp"
+					mountPath: "/tmp"
+				}]
+				resources: {
+					limits: {
+						cpu:    "100m"
+						memory: "30Mi"
+					}
+					requests: {
+						cpu:    "100m"
+						memory: "20Mi"
+					}
+				}
+			}]
+			terminationGracePeriodSeconds: 10
+		}
+	}
+}
+
+_command: [...string]
+
+if !parameters.test {
+	_workload: {
+		apiVersion: "apps/v1"
+		kind:       "Deployment"
+	}
+	_workloadSpec: {
+		selector: matchLabels: name: "\(constants.name)"
+		template: metadata: labels: name: "\(constants.name)"
+		replicas: 1
+	}
+	_command: [
+		"/usr/bin/\(constants.name)",
+		"--enable-leader-election",
+	]
+}
+
+if parameters.test {
+	_workload: {
+		apiVersion: "batch/v1"
+		kind:       "Job"
+	}
+	_workloadSpec: {
+		backoffLimit: 0
+		template: spec: {
+			restartPolicy: "Never"
+			containers: [{
+				name: "operator"
+				env: [{
+					name: "NAMESPACE"
+					valueFrom: fieldRef: fieldPath: "metadata.namespace"
+				}]
+			}]
+		}
+	}
+	_command: [
+		"test.gke-test-cluster-operator-controllers",
+		"-test.v",
+		"-test.timeout=5m",
+		"-resource-prefix=\(parameters.namespace)",
+		"-crd-path=/config/crd",
+	]
+}
+
+WorkloadTemplate :: {
+	kind:       "List"
+	apiVersion: "v1"
+	items: [{
+		apiVersion: "v1"
+		kind:       "ServiceAccount"
+		metadata: {
+			name: "\(constants.name)"
+			labels: name: "\(constants.name)"
+			namespace: "\(parameters.namespace)"
+		}
+	}, {
+		apiVersion: "rbac.authorization.k8s.io/v1beta1"
+		kind:       "ClusterRoleBinding"
+		metadata: {
+			name: "\(parameters.namespace)-\(constants.name)"
+			labels: name: "\(constants.name)"
+		}
+		roleRef: {
+			kind:     "ClusterRole"
+			name:     "\(constants.name)"
+			apiGroup: "rbac.authorization.k8s.io"
+		}
+		subjects: [{
+			kind:      "ServiceAccount"
+			name:      "\(constants.name)"
+			namespace: "\(parameters.namespace)"
+		}]
+	},
+		_workload,
+	]
+}
+
+WorkloadParameters :: {
+	namespace: string
+	image:     string
+	test:      bool
+}
+
+parameters: WorkloadParameters
+template:   WorkloadTemplate
--- operator/test-controllers.sh
+++ operator/test-controllers.sh
@@ -9,7 +9,7 @@ set -o nounset
 
 image="${1}"
 
-name="test-gke-test-cluster-operator-controllers"
+name="gke-test-cluster-operator"
 namespace="${name}-$(date +%s)"
 
 # this is a simple script for runnig controller tests on kind,
@@ -17,80 +17,20 @@ namespace="${name}-$(date +%s)"
 
 echo "INFO: creating test job"
 
-kubectl create --namespace "${namespace}" --filename - <<EOF
-apiVersion: v1
-kind: List
-items:
-  - apiVersion: v1
-    kind: Namespace
-    metadata:
-      name: "${namespace}"
-      labels:
-        test: "${name}"
-  - apiVersion: v1
-    kind: ServiceAccount
-    metadata:
-      name: "${name}"
-      namespace: "${namespace}"
-      labels:
-        test: "${name}"
-  - apiVersion: rbac.authorization.k8s.io/v1beta1
-    kind: ClusterRoleBinding
-    metadata:
-      name: "${namespace}"
-      namespace: "${namespace}"
-      labels:
-        test: "${name}"
-    roleRef:
-      kind: ClusterRole
-      name: cluster-admin
-      apiGroup: rbac.authorization.k8s.io
-    subjects:
-      - kind: ServiceAccount
-        name: "${name}"
-        namespace: "${namespace}"
-  - apiVersion: batch/v1
-    kind: Job
-    metadata:
-      name: "${name}"
-      namespace: "${namespace}"
-      labels:
-        test: "${name}"
-    spec:
-      backoffLimit: 0
-      template:
-        metadata:
-          labels:
-            test: "${name}"
-        spec:
-          serviceAccount: "${name}"
-          tolerations:
-            - effect: NoSchedule
-              operator: Exists
-          restartPolicy: Never
-          dnsPolicy: ClusterFirst
-          volumes:
-          - name: tmp
-            emptyDir: {}
-          containers:
-            - name: test
-              image: "${image}"
-              imagePullPolicy: Never
-              command:
-              - test.gke-test-cluster-operator-controllers
-              - -test.v
-              - -test.timeout=5m
-              - -resource-prefix=\$(NAMESPACE)
-              - -crd-path=/config/crd
-              volumeMounts:
-              - mountPath: /tmp
-                name: tmp
-              env:
-              - name: NAMESPACE
-                valueFrom:
-                  fieldRef:
-                    fieldPath: metadata.namespace
-EOF
+jq -n \
+	--arg image "${image}" \
+  --arg namespace "${namespace}" \
+	  '.instances=[({} | .output="operator-test.json" | .parameters=({} | .namespace="\($namespace)" | .image="\($image)" | .test=true))]' \
+	> config/operator/instances.json
+
+${GOPATH}/bin/kg -input-directory config/operator -output-directory config/operator
+
+kubectl apply --filename="config/rbac/role.yaml"
+
+kubectl create namespace "${namespace}"
+kubectl label namespace "${namespace}" test="${name}"
+
+kubectl create --namespace "${namespace}" --filename="config/operator/operator-test.json"
 
 # these tests should quick and there is no point in streaming logs,
 # completion should be within a minute or two, otherwise there is
@@ -98,30 +38,29 @@ EOF
 
 follow_logs() {
   echo "INFO: streaming container logs"
-  kubectl logs --namespace="${namespace}" --selector="test=${name}" --follow || true
+  kubectl logs --namespace="${namespace}" --selector="name=${name}" --follow || true
 }
 
 troubleshoot() {
   echo "INFO: gatherig additional information that maybe usefull in troubleshooting the failure"
-  kubectl describe pods --namespace="${namespace}" --selector="test=${name}"
+  kubectl describe pods --namespace="${namespace}" --selector="name=${name}"
 }
 
 bail() {
   echo "INFO: cleaning up..."
-  kubectl delete namespace "${namespace}" --wait="false"
+  kubectl delete --wait="false"  --filename="config/operator/operator-test.json"
   # test namespaces are not deleted on test failure, so make sure those are cleaned up here
   kubectl delete namespace --selector="test=${namespace}" --field-selector="status.phase=Active" --wait="false"
-  kubectl delete clusterrolebinding "${namespace}" --wait="false"
   exit "$1"
 }
 
 one_pod_running() {
-  pods=($(kubectl get pods --namespace="${namespace}" --selector="test=${name}" --output="jsonpath={range .items[?(.status.phase == \"Running\")]}{.metadata.name}{\"\n\"}{end}"))
+  pods=($(kubectl get pods --namespace="${namespace}" --selector="name=${name}" --output="jsonpath={range .items[?(.status.phase == \"Running\")]}{.metadata.name}{\"\n\"}{end}"))
   test "${#pods[@]}" -eq 1
 }
 
 one_pod_failed() {
-  pods=($(kubectl get pods --namespace="${namespace}" --selector="test=${name}" --output="jsonpath={range .items[?(.status.phase == \"Failed\")]}{.metadata.name}{\"\n\"}{end}"))
+  pods=($(kubectl get pods --namespace="${namespace}" --selector="name=${name}" --output="jsonpath={range .items[?(.status.phase == \"Failed\")]}{.metadata.name}{\"\n\"}{end}"))
   test "${#pods[@]}" -eq 1
 }
 
@@ -129,19 +68,21 @@ wait_for_pod() {
   echo "INFO: waiting for the test job to start..."
   # kubectl wait job only supports two condtions - complete or failed,
   # so wait for a pod to get schulded
-  until kubectl wait pods --namespace="${namespace}" --selector="test=${name}" --for="condition=PodScheduled" --timeout="2m" 2> /dev/null ; do sleep 0.5 ; done
+  # TODO: this doesn't check if job failed to create pods, which can happen
+  # if there is configuration issue
+  until kubectl wait pods --namespace="${namespace}" --selector="name=${name}" --for="condition=PodScheduled" --timeout="2m" 2> /dev/null ; do sleep 0.5 ; done
   # kubectl wait doesn't support multiple conditions, and `wait -n` is not
   # available in all common versions of bash, so poll pod status instead
   until one_pod_running || one_pod_failed ; do
-    kubectl get pods --namespace="${namespace}" --selector="test=${name}" --show-kind --no-headers
+    kubectl get pods --namespace="${namespace}" --selector="name=${name}" --show-kind --no-headers
     sleep 0.5
   done
 }
 
 container_status() {
   echo "INFO: getting container status..."
-  exit_code="$(kubectl get pods --namespace="${namespace}" --selector="test=${name}" --output="jsonpath={.items[0].status.containerStatuses[0].state.terminated.exitCode}")"
-  echo "INFO: container existed with ${exit_code}"
+  exit_code="$(kubectl get pods --namespace="${namespace}" --selector="name=${name}" --output="jsonpath={.items[0].status.containerStatuses[0].state.terminated.exitCode}")"
+  echo "INFO: container exited with ${exit_code}"
   return "${exit_code}"
 }
 
