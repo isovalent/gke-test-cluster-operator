3077ce60b41a0c6fefd011dffc72e35c4e03227a
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-08-03 20:32:35 +0100

    Implement `client.NewExternalClient`
    This provide a traditional clientset, as well as a REST client that
    has CNRM and v1alpha1 schema.

--- operator/go.mod
+++ operator/go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/onsi/gomega v1.9.0
 	github.com/prometheus/client_golang v1.7.0
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/api v0.25.0
 	k8s.io/api v0.18.5
 	k8s.io/apimachinery v0.18.5
 	k8s.io/client-go v0.18.5
--- operator/pkg/client/client.go
+++ operator/pkg/client/client.go
@@ -10,14 +10,19 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/isovalent/gke-test-cluster-management/operator/api/cnrm"
-
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
+	"google.golang.org/api/container/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/isovalent/gke-test-cluster-management/operator/api/cnrm"
+	"github.com/isovalent/gke-test-cluster-management/operator/api/v1alpha1"
 )
 
 func googleScopes() []string {
@@ -38,35 +43,84 @@ type ClientSetBuilder interface {
 type GKEClientSetBuilder struct{}
 
 func NewClientSetBuilder() (*GKEClientSetBuilder, error) {
-	newGoogleAuthProvider := func(_ string, _ map[string]string, _ rest.AuthProviderConfigPersister) (rest.AuthProvider, error) {
-		ts, err := google.DefaultTokenSource(context.Background(), googleScopes()...)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create google token source: %+v", err)
-		}
-
-		return &googleAuthProvider{tokenSource: ts}, nil
-	}
-	if err := rest.RegisterAuthProviderPlugin(googleAuthPlugin, newGoogleAuthProvider); err != nil {
-		return nil, fmt.Errorf("failed to register %s auth plugin: %v", googleAuthPlugin, err)
+	if err := registerAuthProviderPlugin(); err != nil {
+		return nil, err
 	}
 	return &GKEClientSetBuilder{}, nil
 }
 
 func (GKEClientSetBuilder) NewClientSet(cluster *cnrm.PartialContainerCluster) (kubernetes.Interface, error) {
-	caCert, err := base64.StdEncoding.DecodeString(cluster.Spec.MasterAuth.ClusterCACertificate)
+	config, err := newConfig(cluster.Status.Endpoint, cluster.Spec.MasterAuth.ClusterCACertificate)
 	if err != nil {
-		return nil, fmt.Errorf("error decoding CA certificate: %v", err)
+		return nil, err
 	}
-
-	config := &rest.Config{
-		TLSClientConfig: rest.TLSClientConfig{
-			CAData: []byte(caCert),
-		},
-		Host:         cluster.Status.Endpoint,
-		AuthProvider: &clientcmdapi.AuthProviderConfig{Name: googleAuthPlugin},
-	}
-
 	return kubernetes.NewForConfig(config)
+}
+
+func NewExternalClient(ctx context.Context, project, clusterName string) (kubernetes.Interface, client.Client, error) {
+	scheme := runtime.NewScheme()
+	if err := clientgoscheme.AddToScheme(scheme); err != nil {
+		return nil, nil, err
+	}
+	if err := cnrm.AddToScheme(scheme); err != nil {
+		return nil, nil, err
+	}
+	if err := v1alpha1.AddToScheme(scheme); err != nil {
+		return nil, nil, err
+	}
+
+	ts, err := google.DefaultTokenSource(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to get a token source: %v", err)
+	}
+
+	httpClient := oauth2.NewClient(ctx, ts)
+	gke, err := container.New(httpClient)
+	if err != nil {
+		return nil, nil, fmt.Errorf("could not create GKE API client: %v", err)
+	}
+
+	clusters, err := gke.Projects.Zones.Clusters.List(project, "-").Context(ctx).Do()
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot list cluster in project %q: %v", project, err)
+	}
+
+	matches := 0
+
+	var cluster *container.Cluster
+
+	for _, c := range clusters.Clusters {
+		if c.Name == clusterName {
+			matches++
+			cluster = c
+		}
+	}
+
+	if matches == 0 {
+		return nil, nil, fmt.Errorf("cluster %q could not be found", clusterName)
+	}
+
+	if matches > 1 {
+		return nil, nil, fmt.Errorf("too many clusters using the same name %q (found %d, expected 1)", clusterName, matches)
+	}
+
+	if err := registerAuthProviderPlugin(); err != nil {
+		return nil, nil, err
+	}
+
+	config, err := newConfig(cluster.Endpoint, cluster.MasterAuth.ClusterCaCertificate)
+	if err != nil {
+		return nil, nil, err
+	}
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+	restClient, err := client.New(config, client.Options{Scheme: scheme})
+	if err != nil {
+		return nil, nil, err
+	}
+	return clientSet, restClient, nil
 }
 
 var _ rest.AuthProvider = &googleAuthProvider{}
@@ -82,3 +136,34 @@ func (g *googleAuthProvider) WrapTransport(rt http.RoundTripper) http.RoundTripp
 	}
 }
 func (g *googleAuthProvider) Login() error { return nil }
+
+func newConfig(endpoint, ecodedCACert string) (*rest.Config, error) {
+	caCert, err := base64.StdEncoding.DecodeString(ecodedCACert)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding CA certificate: %v", err)
+	}
+
+	config := &rest.Config{
+		TLSClientConfig: rest.TLSClientConfig{
+			CAData: []byte(caCert),
+		},
+		Host:         endpoint,
+		AuthProvider: &clientcmdapi.AuthProviderConfig{Name: googleAuthPlugin},
+	}
+
+	return config, nil
+}
+
+func registerAuthProviderPlugin() error {
+	newGoogleAuthProvider := func(_ string, _ map[string]string, _ rest.AuthProviderConfigPersister) (rest.AuthProvider, error) {
+		ts, err := google.DefaultTokenSource(context.Background(), googleScopes()...)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create google token source: %+v", err)
+		}
+		return &googleAuthProvider{tokenSource: ts}, nil
+	}
+	if err := rest.RegisterAuthProviderPlugin(googleAuthPlugin, newGoogleAuthProvider); err != nil {
+		return fmt.Errorf("failed to register %s auth plugin: %v", googleAuthPlugin, err)
+	}
+	return nil
+}
