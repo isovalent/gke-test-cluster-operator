1a3867b78c3bc5af50ffc3d7e313399804d68aad
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-08-17 20:42:51 +0100

    Add permissions to access pods

--- operator/controllers/testclustergke_controller.go
+++ operator/controllers/testclustergke_controller.go
@@ -27,6 +27,9 @@ import (
 // +kubebuilder:rbac:groups="batch",resources=jobs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="batch",resources=jobs/status,verbs=get;update;patch
 
+// +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch
+// +kubebuilder:rbac:groups="",resources=pods/status,verbs=get
+
 // +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=configmaps/status,verbs=get;update;patch
 
