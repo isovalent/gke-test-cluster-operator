23830c14ff1be7feaccac7cc2122997a86dff4af
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-08-24 14:39:16 +0100

    Implement defaulting and validation webhooks
    - move defaulting logic from basic config template
    - always pass creation and deletion validation
    - always fail update validation

--- operator/api/v1alpha1/testclustergke_webhook.go
+++ operator/api/v1alpha1/testclustergke_webhook.go
@@ -4,14 +4,15 @@
 package v1alpha1
 
 import (
+	"errors"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
-// log is for logging in this package.
-var testclustergkelog = logf.Log.WithName("testclustergke-resource")
+var log = logf.Log.WithName("testclustergke-resource")
 
 func (r *TestClusterGKE) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).
@@ -19,44 +20,77 @@ func (r *TestClusterGKE) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
 // +kubebuilder:webhook:path=/mutate-clusters-ci-cilium-io-v1alpha1-testclustergke,mutating=true,failurePolicy=fail,groups=clusters.ci.cilium.io,resources=testclustergkes,verbs=create;update,versions=v1alpha1,name=mtestclustergke.kb.io
 
 var _ webhook.Defaulter = &TestClusterGKE{}
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type
 func (r *TestClusterGKE) Default() {
-	testclustergkelog.Info("default", "name", r.Name)
+	log.Info("default", "name", r.Name)
 
-	// TODO(user): fill in your defaulting logic.
+	if r.Spec.Nodes == nil {
+		r.Spec.Nodes = new(int)
+		*r.Spec.Nodes = 2
+	}
+
+	if r.Spec.MachineType == nil {
+		r.Spec.MachineType = new(string)
+		*r.Spec.MachineType = "n1-standard-4"
+	}
+
+	if r.Spec.Project == nil {
+		r.Spec.Project = new(string)
+		*r.Spec.Project = "cilium-ci"
+	}
+
+	if r.Spec.Location == nil {
+		r.Spec.Location = new(string)
+		*r.Spec.Location = "europe-west2-b"
+	}
+
+	if r.Spec.Region == nil {
+		r.Spec.Region = new(string)
+		*r.Spec.Region = "europe-west2"
+	}
+
+	if r.Spec.JobSpec == nil {
+		r.Spec.JobSpec = &TestClusterGKEJobSpec{}
+	}
+
+	if r.Spec.JobSpec.Runner == nil {
+		r.Spec.JobSpec.Runner = &TestClusterGKEJobRunnerSpec{
+			Image:     new(string),
+			InitImage: new(string),
+		}
+	}
+
+	if r.Spec.JobSpec.Runner.Image == nil {
+		*r.Spec.JobSpec.Runner.Image = "docker.io/google/cloud-sdk:slim@sha256:a2bade78228faad59a16c36d440f10cfef58a6055cd997d19e258c59c78a409a"
+	}
+
+	if r.Spec.JobSpec.Runner.InitImage == nil {
+		*r.Spec.JobSpec.Runner.InitImage = "quay.io/isovalent/gke-test-cluster-job-runner-init:28c3b8e6218d145398f78e1343d95b16012fc179"
+	}
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-// +kubebuilder:webhook:verbs=create;update,path=/validate-clusters-ci-cilium-io-v1alpha1-testclustergke,mutating=false,failurePolicy=fail,groups=clusters.ci.cilium.io,resources=testclustergkes,versions=v1alpha1,name=vtestclustergke.kb.io
+// +kubebuilder:webhook:verbs=create;update;delete,path=/validate-clusters-ci-cilium-io-v1alpha1-testclustergke,mutating=false,failurePolicy=fail,groups=clusters.ci.cilium.io,resources=testclustergkes,versions=v1alpha1,name=vtestclustergke.kb.io
 
 var _ webhook.Validator = &TestClusterGKE{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *TestClusterGKE) ValidateCreate() error {
-	testclustergkelog.Info("validate create", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object creation.
+	log.Info("validate create", "namespace", r.Namespace, "name", r.Name)
 	return nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *TestClusterGKE) ValidateUpdate(old runtime.Object) error {
-	testclustergkelog.Info("validate update", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object update.
-	return nil
+	log.Info("validate update", "namespace", r.Namespace, "name", r.Name)
+	return errors.New("updates are not supported")
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *TestClusterGKE) ValidateDelete() error {
-	testclustergkelog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
+	log.Info("validate delete", "namespace", r.Namespace, "name", r.Name)
 	return nil
 }
--- operator/config/templates/basic/defaults.go
+++ operator/config/templates/basic/defaults.go
@@ -13,28 +13,9 @@ func NewDefaults() *v1alpha1.TestClusterGKE {
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: "default",
 		},
-		Spec: v1alpha1.TestClusterGKESpec{
-			Nodes:       new(int),
-			MachineType: new(string),
-			Project:     new(string),
-			Location:    new(string),
-			Region:      new(string),
-			JobSpec: &v1alpha1.TestClusterGKEJobSpec{
-				Runner: &v1alpha1.TestClusterGKEJobRunnerSpec{
-					Image:     new(string),
-					InitImage: new(string),
-				},
-			},
-		},
 	}
 
-	*defaults.Spec.Nodes = 2
-	*defaults.Spec.MachineType = "n1-standard-4"
-	*defaults.Spec.Project = "cilium-ci"
-	*defaults.Spec.Location = "europe-west2-b"
-	*defaults.Spec.Region = "europe-west2"
-	*defaults.Spec.JobSpec.Runner.Image = "docker.io/google/cloud-sdk:slim@sha256:a2bade78228faad59a16c36d440f10cfef58a6055cd997d19e258c59c78a409a"
-	*defaults.Spec.JobSpec.Runner.InitImage = "quay.io/isovalent/gke-test-cluster-job-runner-init:28c3b8e6218d145398f78e1343d95b16012fc179"
+	defaults.Default()
 
 	return defaults
 }
