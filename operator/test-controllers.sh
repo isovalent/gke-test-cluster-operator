bfa249d38937df691aa54e095a77ca764d74f07b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-08-25 16:56:50 +0100

    Enhance cert-manager wait logic

--- operator/test-controllers.sh
+++ operator/test-controllers.sh
@@ -43,7 +43,9 @@ one_pod_failed() {
 
 wait_for_cert_manager() {
   echo "INFO: waiting for cert-manager to start..."
+  kubectl wait deployment --namespace="cert-manager" --for="condition=Available" cert-manager-webhook cert-manager-cainjector cert-manager
   kubectl wait pods --namespace="cert-manager" --for="condition=Ready" --all --timeout=2m
+  kubectl wait apiservice --for="condition=Available" v1beta1.cert-manager.io v1beta1.acme.cert-manager.io
   until kubectl get secret --namespace="cert-manager" cert-manager-webhook-ca 2> /dev/null ; do sleep 0.5 ; done
 }
 
