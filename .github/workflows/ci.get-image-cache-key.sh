0f2411ea9083a62dffa2704da28f368b0fef0e75
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-01-15 14:51:32 +0000

    Fix minor issue detected by shellcheck

--- .github/workflows/ci.get-image-cache-key.sh
+++ .github/workflows/ci.get-image-cache-key.sh
@@ -13,4 +13,4 @@ fi
 
 # use git hash-object purelly out of convenient, as e.g. with sha256sum output needs to be parsed
 event_hash="$(git hash-object "${GITHUB_EVENT_PATH}")"
-printf "::set-output name=key::%s-%s\n" "${event_hash}"
+printf "::set-output name=key::%s\n" "${event_hash}"
